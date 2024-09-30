067c853f383eeeb8d00dcf3ff090d8f49c02dd7f
Ilene Varela-Zul <ilenevarela03@gmail.com>
2024-09-30 15:32:03 -0700

    Basic stuff added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Main from "./components/Main.js";
+import NavBar from "./components/Navbar.js"
 
 function App() {
   return(
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Main />}/>
       </Routes>
